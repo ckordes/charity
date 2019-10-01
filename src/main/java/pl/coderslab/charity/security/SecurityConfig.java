f971f43c13f5af45cb1e9483321adb6fc3cfe44c
ckordes <ckordes@gmail.com>
2019-10-01 23:05:27 +0200

    added spring security to manage access

--- src/main/java/pl/coderslab/charity/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package pl.coderslab.charity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().logout().logoutSuccessUrl("/")
                 .permitAll()
                 .and().exceptionHandling().accessDeniedPage("/403");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
